ca7102d7e81b80efa59cb3fbc07c74e98f6dcfcd
Lutaaya Jamil <kunoomaking@gmail.com>
2023-03-10 16:45:50 +0300

    Bug free This one is free from majority of the errors, infact I haven't seen any....Though I haven't looked much into memory management.

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Weaver_game_c
+This program is attempting to solve the problem in the attached pdf. [Program 2_ Weaver(1).pdf]
+The textfile used is also included "words.txt"
--- main.c
+++ main.c
@@ -2,109 +2,111 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define WORDS_FILE_PATH "words.txt" // path to the file containing words
+struct StringArray
+{
+    char **words;
+    int size;
+    int capacity;
+};
 
+void initializeStringArray(struct StringArray *arr)
+{
+    arr->words = malloc(10 * sizeof(char *));
+    arr->size = 0;
+    arr->capacity = 10;
+}
+
+void addString(struct StringArray *arr, const char *word)
+{
+    if (arr->size == arr->capacity)
+    {
+        // Double the capacity of the array if it's full
+        arr->capacity *= 2;
+        arr->words = realloc(arr->words, arr->capacity * sizeof(char *));
+    }
+
+    // Allocate memory for the new string and copy the contents of `word` into it
+    arr->words[arr->size] = malloc(strlen(word) + 1);
+    strcpy(arr->words[arr->size], word);
+
+    // Increment the size of the array
+    arr->size++;
+}
+
+void freeStringArray(struct StringArray *arr)
+{
+    for (int i = 0; i < arr->size; i++)
+    {
+        free(arr->words[i]);
+    }
+    free(arr->words);
+    arr->size = 0;
+    arr->capacity = 0;
+}
 
 int main()
 {
-    srand( 1);
 
+    srand(1);
+    int word_len = 0;
+    struct StringArray arr;
+    FILE *fp;
 
-    int word_len;
     printf("Weaver is a game where you try to find a way to get from the starting word to the ending word.\nYou can change only one letter at a time, and each word along the way must be a valid word.\nEnjoy!");
 two:
-
-    printf("\n\nHow many letters do you want to have in the words? ");
-    FILE* fp = fopen(WORDS_FILE_PATH, "r"); // open file in read mode
+    
+    fp = fopen("words.txt", "r");
     if (fp == NULL)
     {
-        printf("Error: Failed to open file %s\n", WORDS_FILE_PATH);
-        return 1;
+        printf("Could not open file\n");
+        exit(1);
     }
+    initializeStringArray(&arr);
+    printf("\n\nHow many letters do you want to have in the words? ");
     scanf("%d", &word_len);
 
-
-    char* word = (char*) malloc(sizeof(char) * (word_len + 1)); // allocate space for the word
-    if (word == NULL)
-    {
-        printf("Error: Failed to allocate memory for word\n");
-        return 1;
-    }
-
-    int word_count = 0;
-    int max_words = 10; // initial size of array to store words
-    char** words = (char**) malloc(sizeof(char*) * max_words); // allocate space for array of words
-    if (words == NULL)
-    {
-        printf("Error: Failed to allocate memory for words array\n");
-        return 1;
-    }
-
-    // read each word from file and add it to the array if its length is equal to the user input
-    while (fscanf(fp, "%s", word) != EOF)
+    char word[100];
+    while (fscanf(fp, "%s", word) == 1)
     {
         if (strlen(word) == word_len)
         {
-            words[word_count] = (char*) malloc(sizeof(char) * (word_len + 1)); // allocate space for word
-            if (words[word_count] == NULL)
-            {
-                printf("Error: Failed to allocate memory for word\n");
-                return 1;
-            }
-            strcpy(words[word_count], word); // copy word to array
-            word_count++;
-
-            // dynamically grow array if needed
-            if (word_count == max_words)
-            {
-                max_words *= 2; // double the size of array
-                char** new_words = (char**) realloc(words, sizeof(char*) * max_words);
-                if (new_words == NULL)
-                {
-                    printf("Error: Failed to reallocate memory for words array\n");
-                    return 1;
-                }
-                words = new_words;
-            }
+            addString(&arr, word);
         }
     }
-
     char start_word[20];
     char end_word[20];
-    printf("Number of %d-letter words found: %d.\n\n", word_len, word_count);
+    printf("Number of %d-letter words found: %d.\n\n", word_len, arr.size);
 
     int len_start = 0;
     int len_end = 0;
 one:
-    while(len_end != word_len || len_start != word_len)
+    while (len_end != word_len || len_start != word_len)
     {
 
-
         printf("Enter starting and ending words, or 'r' for either for a random word: ");
-        scanf("%s %s", start_word, end_word );
+        scanf("%s %s", start_word, end_word);
 
-        //start of length check
         len_start = strlen(start_word);
         len_end = strlen(end_word);
 
         int found = 0;
-        if(strcmp(start_word, "r") == 0 && strcmp(end_word, "r") == 0)
+        if (strcmp(start_word, "r") == 0 && strcmp(end_word, "r") == 0)
         {
-            // generate two random words of length word_len
-            int random_index1 = rand() % word_count; // get a random index within the range of word_count
-            int random_index2 = rand() % word_count; // get another random index within the range of word_count
-            char* random_word1 = words[random_index1]; // get the word at the first random index
-            char* random_word2 = words[random_index2]; // get the word at the second random index
-            strcpy(start_word, random_word1); // replace start_word with random_word1
-            strcpy(end_word, random_word2); // replace end_word with random_word2
+            // generate two random words of the length set by player.
+            int random_index1 = rand() % arr.size;         // get a random index within the range of words available in the array.
+            int random_index2 = rand() % arr.size;         // get another random index.
+            char *random_word1 = arr.words[random_index1]; // get the word at the first random index
+            char *random_word2 = arr.words[random_index2]; // get the word at the second random index
+            strcpy(start_word, random_word1);              // replace start_word with random_word1
+            strcpy(end_word, random_word2);                // replace end_word with random_word2
             len_start = strlen(start_word);
             len_end = strlen(end_word);
         }
-        else if(strcmp(start_word, "r") == 0 || strcmp(end_word, "r") == 0)
+        else if (strcmp(start_word, "r") == 0 || strcmp(end_word, "r") == 0)
         {
             // generate a random word of length word_len
-            int random_index = rand() % word_count; // get a random index within the range of word_count
-            char* random_word = words[random_index]; // get the word at the random index
+            int random_index = rand() % arr.size;        // get a random index within the range of word_count
+            char *random_word = arr.words[random_index]; // get the word at the random index
             if (strcmp(start_word, "r") == 0)
             {
                 strcpy(start_word, random_word);
@@ -117,30 +119,26 @@ one:
                 len_start = strlen(start_word);
                 len_end = strlen(end_word);
             }
-
-
-
         }
 
         else
         {
-            if(len_start != word_len)
+            if (len_start != word_len)
             {
                 printf("Your word, '%s', is not a %d-letter word. Try again.\n", start_word, word_len);
                 continue;
             }
 
-            if(len_end != word_len)
+            if (len_end != word_len)
             {
                 printf("Your word, '%s', is not a %d-letter word. Try again.\n", end_word, word_len);
                 continue;
             }
 
-            // printf("Checking if %s is in the array of words...\n", start_word);
             found = 0;
-            for (int i = 0; i < word_count; i++)
+            for (int i = 0; i < arr.size; i++)
             {
-                if (strcmp(start_word, words[i]) == 0)
+                if (strcmp(start_word, arr.words[i]) == 0)
                 {
                     found = 1; // set flag if word is found
                     break;
@@ -155,9 +153,9 @@ one:
             }
 
             found = 0;
-            for (int i = 0; i < word_count; i++)
+            for (int i = 0; i < arr.size; i++)
             {
-                if (strcmp(end_word, words[i]) == 0)
+                if (strcmp(end_word, arr.words[i]) == 0)
                 {
                     found = 1; // set flag if word is found
                     break;
@@ -186,9 +184,7 @@ one:
     int num = 1;
     int done = 0;
     char received_word[word_len];
-// strcpy(received_word, start_word);
-    char dup[100];
-    while(1)
+    while (1)
     {
         if (strcmp(start_word, end_word) == 0)
         {
@@ -196,7 +192,6 @@ one:
             break;
         }
 
-        //strcpy(dup, received_word);
         printf("\n%d. Previous word is '%s'. Goal word is '%s'. Next word: ", num, start_word, end_word);
         scanf("%s", received_word);
         count = 0;
@@ -204,12 +199,12 @@ one:
         {
             goto start;
         }
-        else if(strlen(received_word) == word_len)
+        else if (strlen(received_word) == word_len)
         {
             int found = 0;
-            for (int i = 0; i < word_count; i++)
+            for (int i = 0; i < arr.size; i++)
             {
-                if (strcmp(received_word, words[i]) == 0)
+                if (strcmp(received_word, arr.words[i]) == 0)
                 {
                     found = 1; // set flag if word is found
                     break;
@@ -223,23 +218,22 @@ one:
             else
             {
 
-                for(i=0; i<word_len; i++)
+                for (i = 0; i < word_len; i++)
                 {
-                    if(start_word[i] != received_word[i])
+                    if (start_word[i] != received_word[i])
                     {
                         count++;
                     }
                 }
-                if (count==0)
+                if (count == 0)
                 {
                     printf("Your word, '%s', is not exactly 1 character different. Try again.\n", received_word);
                     continue;
                 }
 
-
-                else if(count <= 1)
+                else if (count <= 1)
                 {
-                    num ++;
+                    num++;
                     if (strcmp(start_word, end_word) == 0)
                     {
                         done = 1;
@@ -255,13 +249,7 @@ one:
                 {
                     printf("Your word, '%s', is not exactly 1 character different. Try again.\n", received_word);
                     continue;
-
                 }
-
-
-
-
-
             }
         }
         else
@@ -273,14 +261,10 @@ one:
 
     if (done)
     {
-        printf("Congratulations! You changed '%s' into '%s' in %d moves.\n", store, end_word, num-1);
+        printf("Congratulations! You changed '%s' into '%s' in %d moves.\n", store, end_word, num - 1);
     }
 
-    /*Enter: 1 to play again,
-    2 to change the number of letters in the words and then play again, or
-    3 to exit the program.
-
-    */
+// Prompt user to eiher play again or quit
 start:
     printf("\nEnter: \t1 to play again,\n");
     printf("\t\t2 to change the number of letters in the words and then play again, or\n");
@@ -289,53 +273,30 @@ start:
 
     int select = 0;
     scanf("%d", &select);
-    if(select == 1)
+    if (select == 1)
     {
         len_start = 0;
         len_end = 0;
         goto one;
     }
-    else if(select == 2)
+    else if (select == 2)
     {
+        fclose(fp);
+        freeStringArray(&arr);
         goto two;
     }
-    else if(select == 3)
+    else if (select == 3)
     {
         goto three;
     }
 
-
-
-
-
-
-
-
 three:
-
-
-
-    /*
-               if(strcmp(start_word, "r") == 0) {
-        printf("Start word is r");
-    }
-           else{
-            printf("It is %s", start_word);
-           }
-    */
-
-
-
-
-
-
-    // free memory
-
-    free(words);
+    
     printf("\nThanks for playing!\n");
     printf("Exiting...");
+    fclose(fp);
 
-    fclose(fp); // close file
+    freeStringArray(&arr);
 
     return 0;
 }
